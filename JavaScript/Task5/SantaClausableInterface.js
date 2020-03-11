bdd7a990d13a4e5b7fc019704667babc903123a5
AnchalAgrawal4 <42673200+AnchalAgrawal4@users.noreply.github.com>
2020-03-11 17:42:08 +0530

    Revert "Js"

--- JavaScript/Task5/SantaClausableInterface.js
+++
@@ -1,6 +0,0 @@
-//http://www.codewars.com/kata/santaclausable-interface
-
-function isSantaClausable(obj)
-{
-return ['sayHoHoHo','distributeGifts','goDownTheChimney'].every(function(method){return typeof obj[method] == function})
-}
