9f62f74ba4992cddc543a07e5973e5677f3fac34
AnchalAgrawal4 <42673200+AnchalAgrawal4@users.noreply.github.com>
2020-03-20 19:18:44 +0530

    Update extract-nested-object-reference.js

--- JavaScript/Task5/extract-nested-object-reference.js
+++ JavaScript/Task5/extract-nested-object-reference.js
@@ -1,10 +1,10 @@
 //http://www.codewars.com/kata/extract-nested-object-reference
 
 Object.prototype.hash = function(string) {
-  var properties = string.split(".");
+  const properties = string.split(".");
   var myObj = this;
   for (let i = 0; i < properties.length; i++) {
-    !!myObj && (myObj = myObj[properties[i]]);
+    Boolean(myObj) && (myObj = myObj[properties[i]]);
   }
   return myObj;
 };
